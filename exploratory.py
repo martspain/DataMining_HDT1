ce7cf6aebdb9e84dd0c0d8aa8c6a1f19a812b657
Olivverde <dele19270@uvg.edu.gt>
2022-02-02 13:43:41 -0600

    First commit

---
+++ exploratory.py
@@ -0,0 +1,8 @@
+# Universidad del Valle de Guatemala
+# Mineria de Datos
+# HDT1-Exploratory Analysis
+#------------------------------------
+# Sofia Rueda    19xxx
+# Martin Espana  19xxx
+# Oliver de Leon 19270
+
