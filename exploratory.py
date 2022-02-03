e50e6af832f65748c12cf517026d8f276cbe6741
Olivverde <dele19270@uvg.edu.gt>
2022-02-02 19:37:42 -0600

    films-per-year currently functional

--- exploratory.py
+++ exploratory.py
@@ -6,8 +6,12 @@
 # Martin Espana  19xxx
 # Oliver de Leon 19270
 
+import datetime
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from reader import reader as Reader
+
 
 class main(object):
 
@@ -20,10 +24,22 @@ class main(object):
 
     # Films-per-year and top year film production amount
     def films_per_year(self):
-        df = self.df['releaseDate']
+        df = self.df
+        # Turning releaseDate column into an actual datetime type column, then extracting just the year
+        df['releaseDate'] = pd.to_datetime(df['releaseDate'], errors='coerce').dt.year
+        # Grouping releaseDate by size, merging it into new "count" column.
+        df = df.groupby(df['releaseDate']).size().to_frame('count')
+        df = df.sort_values('count', ascending=False).head(25)
+        # DataFrame is sortered, then the top value name it's extracted
+        maxYearProd = df.iloc[0].name
+        
+        print("-------------------------")
+        print("Top year production:",maxYearProd)
+        print("-------------------------")
         print(df.head())
-        pass
 
+        ax = df.plot.bar(y='count', use_index=True)
+        plt.show()
 
 exp = main('movies.csv')
 exp.films_per_year()
